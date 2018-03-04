efd7e887b8b78064da8f3f30192ddd6ed94078af
joachimharms <joachimharms77@gmail.com>
2018-03-04 23:17:14 +0100

    Add project files.

---
+++ S01_L04_HelloWorld/S01_L04_HelloWorld/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace S01_L04_HelloWorld
+{
+	public partial class App : Application
+	{
+		public App ()
+		{
+			InitializeComponent();
+
+			MainPage = new S01_L04_HelloWorld.MainPage();
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
