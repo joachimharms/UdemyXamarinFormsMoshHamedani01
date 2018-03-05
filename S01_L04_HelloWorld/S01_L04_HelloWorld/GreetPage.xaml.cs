56bed081cfeaae94c0e14128c68fdc5190296323
joachimharms <joachimharms77@gmail.com>
2018-03-06 00:02:56 +0100

    Button without Clicked Event -done

---
+++ S01_L04_HelloWorld/S01_L04_HelloWorld/GreetPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace S01_L04_HelloWorld
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class GreetPage : ContentPage
+	{
+		public GreetPage ()
+		{
+		    //void Handle_Clicked(object sender, System.EventArgs e)
+		    //{
+		    //    throw new NotImplementedException();
+		    //}
+			InitializeComponent ();
+            
+		}
+	}
+}
